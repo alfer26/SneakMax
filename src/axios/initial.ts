aff414ffd84682d202690b83e83aa5ef56186174
Ярослав Скорынин <yaroslav.skorynin@mail.ru>
2025-05-05 18:47:49 +0300

    Deploy

--- src/axios/initial.ts
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-export const initial = axios.create({
-    baseURL: 'https://232c2832218a4e04.mokky.dev/',
-});
--- src/hooks/useAppDispatch.ts
+++
@@ -1,4 +0,0 @@
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "../Types";
-
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
